3f9da62ba20e9a1b1529adedf2398fd40d070b15
Oliver Schmid <oliver.schmid@epfl.ch>
2021-03-03 11:19:49 +0100

    move token handler implementations to accessible package

--- kg_core/__communication.py
+++ kg_core/__communication.py
@@ -46,37 +46,6 @@ class TokenHandler(ABC):
                     self._auth_endpoint = auth_endpoint["data"]["endpoint"]
 
 
-class SimpleToken(TokenHandler):
-
-    def __init__(self, token: str):
-        super(SimpleToken, self).__init__()
-        self.__simple_token = token
-
-    def _fetch_token(self):
-        return self.__simple_token
-
-
-class ClientCredentials(TokenHandler):
-
-    def __init__(self, client_id: str, client_secret: str):
-        super(ClientCredentials, self).__init__()
-        self.__client_id = client_id
-        self.__client_secret = client_secret
-
-    def _fetch_token(self):
-        if self._auth_endpoint and self.__client_id and self.__client_secret:
-            token_response = requests.post(self._auth_endpoint, data={
-                "grant_type": "client_credentials",
-                "client_id": self.__client_id,
-                "client_secret": self.__client_secret
-            })
-            if token_response.status_code == 200:
-                token = token_response.json()
-                if token and "access_token" in token:
-                    return token["access_token"]
-            return None
-
-
 class RequestsWithTokenHandler(ABC):
     def __init__(self, endpoint: str, token_handler: TokenHandler):
         self.token_handler = token_handler
---
+++ kg_core/oauth.py
@@ -0,0 +1,46 @@
+#   Copyright (c) 2018, EPFL/Human Brain Project PCO
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import requests
+from kg_core.__communication import TokenHandler
+
+
+class SimpleToken(TokenHandler):
+
+    def __init__(self, token: str):
+        super(SimpleToken, self).__init__()
+        self.__simple_token = token
+
+    def _fetch_token(self):
+        return self.__simple_token
+
+
+class ClientCredentials(TokenHandler):
+
+    def __init__(self, client_id: str, client_secret: str):
+        super(ClientCredentials, self).__init__()
+        self.__client_id = client_id
+        self.__client_secret = client_secret
+
+    def _fetch_token(self):
+        if self._auth_endpoint and self.__client_id and self.__client_secret:
+            token_response = requests.post(self._auth_endpoint, data={
+                "grant_type": "client_credentials",
+                "client_id": self.__client_id,
+                "client_secret": self.__client_secret
+            })
+            if token_response.status_code == 200:
+                token = token_response.json()
+                if token and "access_token" in token:
+                    return token["access_token"]
+            return None
