ddc7defacd09b9a2092939003f3f3d4cd8f97fbd
Oliver Schmid <oliver.schmid@epfl.ch>
2021-03-03 10:08:37 +0100

    add kg result and pagination mechanism

--- kg_core/kg.py
+++ kg_core/kg.py
@@ -13,6 +13,7 @@
 #   limitations under the License.
 import threading
 from abc import ABC, abstractmethod
+from copy import deepcopy
 
 import requests
 
@@ -79,6 +80,35 @@ class ClientCredentials(TokenHandler):
             return None
 
 
+class KGResult(object):
+
+    def __init__(self, json:dict, request_args:dict, payload):
+        self.payload = json
+        self.request_args = request_args
+        self.request_payload = payload
+
+    def data(self):
+        return self.payload["data"] if "data" in self.payload else None
+
+    def message(self):
+        return self.payload["message"] if "message" in self.payload else None
+
+    def start_time(self):
+        return self.payload["startTime"] if "startTime" in self.payload else None
+
+    def duration_in_ms(self):
+        return self.payload["durationInMs"] if "durationInMs" in self.payload else None
+
+    def total(self):
+        return self.payload["total"] if "total" in self.payload else 0
+
+    def size(self):
+        return self.payload["size"] if "size" in self.payload else 0
+
+    def start_from(self):
+        return self.payload["from"] if "from"  in self.payload else 0
+
+
 class RequestsWithTokenHandler(ABC):
     def __init__(self, endpoint: str, token_handler: TokenHandler):
         self.token_handler = token_handler
@@ -100,6 +130,9 @@ class RequestsWithTokenHandler(ABC):
             'url': absolute_path,
             'params': params
         }
+        return self._do_request(args, payload)
+
+    def _do_request(self, args, payload):
         self._set_headers(args, False)
         if payload is not None:
             args['json'] = payload
@@ -107,7 +140,9 @@ class RequestsWithTokenHandler(ABC):
         if r.status_code == 401:
             self._set_headers(args, True)
             r = requests.request(**args)
-        return r
+        args_clone = deepcopy(args)
+        del args_clone["headers"]
+        return KGResult(r.json(), args_clone, payload)
 
     def get(self, path: str, params: dict):
         return self._request("GET", path, None, params)
@@ -145,18 +180,34 @@ class KGv3(RequestsWithTokenHandler):
     def __init__(self, host: str, token_handler: TokenHandler):
         super(KGv3, self).__init__(f"https://{host}/{KGv3.KG_VERSION}", token_handler)
 
+    def next_page(self, result:KGResult):
+        remaining_items = result.total()-(result.start_from()+result.size())
+        if remaining_items>0:
+            if result.request_args:
+                new_args = deepcopy(result.request_args)
+                if not "params" in new_args:
+                    new_args["params"] = {}
+                new_args["params"]["from"] = result.start_from()+result.size()
+                return self._do_request(new_args, result.request_payload)
+        return None
+
     def queries(self, query: dict, stage: Stage):
         return self.post("/queries", query, {"stage": stage})
 
     def instances(self, stage: Stage, target_type: str, space: str = None, search_by_label: str = None, response_configuration: ResponseConfiguration = ResponseConfiguration(),
-                  pagination: Pagination = Pagination()):
+                  pagination: Pagination = Pagination()) -> KGResult:
         return self.get("/instances",
                         {
-                             "stage": stage,
-                             "type": target_type,
-                             "space": space,
-                             "searchByLabel": search_by_label,
-                             "returnPayload": response_configuration.return_payload,
-                             "returnPermissions": response_configuration.return_permissions,
-                             "returnAlternatives": response_configuration.return_alternatives
+                            "stage": stage,
+                            "type": target_type,
+                            "space": space,
+                            "searchByLabel": search_by_label,
+                            "returnPayload": response_configuration.return_payload,
+                            "returnPermissions": response_configuration.return_permissions,
+                            "returnAlternatives": response_configuration.return_alternatives,
+                            "returnEmbedded": response_configuration.return_embedded,
+                            "returnIncomingLinks": response_configuration.return_incoming_links,
+                            "sortByLabel": response_configuration.sort_by_label,
+                            "from": pagination.start_from,
+                            "size": pagination.size
                          })
