6a26fd087c495444874abcbd7b1dfc7f0dda97a5
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-09-21 14:37:30 +0200

    set returnEmbedded to true by default

--- kg_core/models.py
+++ kg_core/models.py
@@ -19,6 +19,25 @@
 #  (Human Brain Project SGA1, SGA2 and SGA3).
 #
 #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This open source software code was developed in part or in whole in the
+#  Human Brain Project, funded from the European Union's Horizon 2020
+#  Framework Programme for Research and Innovation under
+#  Specific Grant Agreements No. 720270, No. 785907, and No. 945539
+#  (Human Brain Project SGA1, SGA2 and SGA3).
+#
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
@@ -46,7 +65,7 @@ class Stage(str):
 
 class ResponseConfiguration(object):
 
-    def __init__(self, return_payload: bool = True, return_permissions: bool=False, return_alternatives: bool=False, return_embedded: bool=False, return_incoming_links: bool=False, sort_by_label: bool=False):
+    def __init__(self, return_payload: bool = True, return_permissions: bool=False, return_alternatives: bool=False, return_embedded: bool=True, return_incoming_links: bool=False, sort_by_label: bool=False):
         self.return_payload = return_payload
         self.return_permissions = return_permissions
         self.return_alternatives = return_alternatives
