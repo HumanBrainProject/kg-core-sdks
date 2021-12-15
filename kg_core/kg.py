b07f52ff05d7d46be6136e2bb6b34e91a836a469
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-12-15 16:40:32 +0100

    add get instances by ids

--- kg_core/kg.py
+++ kg_core/kg.py
@@ -19,6 +19,25 @@
 #  (Human Brain Project SGA1, SGA2 and SGA3).
 #
 #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This open source software code was developed in part or in whole in the
+#  Human Brain Project, funded from the European Union's Horizon 2020
+#  Framework Programme for Research and Innovation under
+#  Specific Grant Agreements No. 720270, No. 785907, and No. 945539
+#  (Human Brain Project SGA1, SGA2 and SGA3).
+#
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
@@ -114,6 +133,19 @@ class KGv3(RequestsWithTokenHandler):
                              "returnEmbedded": response_configuration.return_embedded,
                              "returnIncomingLinks": response_configuration.return_incoming_links,
                              "sortByLabel": response_configuration.sort_by_label
+                         })
+
+    def get_instances_by_ids(self, stage: Stage, ids: List[str],
+                             response_configuration: ResponseConfiguration = ResponseConfiguration()):
+        return self.post(path="/instancesByIds",
+                         payload=ids,
+                         params={
+                             "stage": stage,
+                             "returnPayload": response_configuration.return_payload,
+                             "returnPermissions": response_configuration.return_permissions,
+                             "returnAlternatives": response_configuration.return_alternatives,
+                             "returnEmbedded": response_configuration.return_embedded,
+                             "returnIncomingLinks": response_configuration.return_incoming_links
                          })
 
     def get_instances(self, stage: Stage, target_type: str, space: Optional[str] = None, search_by_label: Optional[str] = None, response_configuration: ResponseConfiguration = ResponseConfiguration(),
