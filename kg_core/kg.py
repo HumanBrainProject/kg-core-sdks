a55f185c4972adb80f134871cd917c3690bfe670
gdenerva <gilles.denervaud@ebrains.eu>
2022-01-10 14:15:58 +0100

    add move_instance_to_another_space
    Co-authored-by: Ioannis Tsanaktsidis <tsanakts.10@gmail.com>

--- kg_core/kg.py
+++ kg_core/kg.py
@@ -19,36 +19,6 @@
 #  (Human Brain Project SGA1, SGA2 and SGA3).
 #
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#  http://www.apache.org/licenses/LICENSE-2.0.
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
-#  This open source software code was developed in part or in whole in the
-#  Human Brain Project, funded from the European Union's Horizon 2020
-#  Framework Programme for Research and Innovation under
-#  Specific Grant Agreements No. 720270, No. 785907, and No. 945539
-#  (Human Brain Project SGA1, SGA2 and SGA3).
-#
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
 import uuid
 from typing import Any, Dict, List, Optional, Union, cast
 from uuid import UUID
@@ -220,6 +190,16 @@ class KGv3(RequestsWithTokenHandler):
                              #"sortByLabel": response_configuration.sort_by_label,
                              "deferInference": defer_inference,
                              "normalizePayload": normalize_payload
+                         })
+
+    def move_instance_to_another_space(self, instance_id: UUID, space, response_configuration: ResponseConfiguration = ResponseConfiguration()):
+        return self.put(path = f"/instances/{instance_id}/spaces/{space}", params={
+                             "returnPayload": response_configuration.return_payload,
+                             "returnPermissions": response_configuration.return_permissions,
+                             "returnAlternatives": response_configuration.return_alternatives,
+                             "returnEmbedded": response_configuration.return_embedded,
+                             "returnIncomingLinks": response_configuration.return_incoming_links
+                             #"incomingLinksPageSize": response_configuration.incoming_links_page_size
                          })
 
     def deprecate_instance(self, instance_id: UUID) -> KGResult:
