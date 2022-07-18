da05e783573a1593a8b821a0a329b4fda89b8cfd
Alexis Durieux <alexis.durieux@ebrains.eu>
2022-07-18 13:57:32 +0200

    Update generator.py with some more typings:wq

--- generator.py
+++ generator.py
@@ -20,17 +20,17 @@
 #
 
 import json
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
-import pdoc
 import requests
 import re
 
 from kg_core.request import ResponseConfiguration, ExtendedResponseConfiguration, Pagination
 from jinja2 import Environment, PackageLoader, select_autoescape
 
-class ClientGenerator(object):
+APPLICATION_JSON = "application/json"
 
+class ClientGenerator(object):
     keyword_translations = {
         "global": "is_global",
         "from": "start",
@@ -39,7 +39,6 @@ class ClientGenerator(object):
         "id": "instance_id",
         "property": "property_name"
     }
-
     specs = ["0%20simple", "1%20advanced"]
     admin_spec = "2%20admin"
     target = "kg_core/kg.py"
@@ -50,7 +49,7 @@ class ClientGenerator(object):
         self.id_namespace:str = id_namespace
         self.default_client_id_for_device_flow = default_client_id_for_device_flow
 
-    def generate(self):
+    def generate(self) -> None:
         env = Environment(
             loader=PackageLoader("generator"),
             autoescape=select_autoescape()
@@ -58,18 +57,18 @@ class ClientGenerator(object):
         template = env.get_template("kg.py.j2")
         api_version = None
 
-        all_specs = []
-        all_schemas = {}
-        for spec in self.specs:
-            api_spec = requests.get(f"{self.spec_root}{spec}").json()
+        all_specs: List[Dict[str, Dict[str, Any]]] = []
+        all_schemas  = {}
+        for self_spec in self.specs:
+            api_spec = requests.get(f"{self.spec_root}{self_spec}").json()
             paths = api_spec["paths"]
-            schemas = api_spec["components"]["schemas"] if "components" in api_spec and "schemas" in api_spec["components"] else {}
+            schemas: Dict[str, Dict[Any, Any]] = api_spec["components"]["schemas"] if "components" in api_spec and "schemas" in api_spec["components"] else {}
             all_schemas.update(schemas)
             all_specs.append(paths)
 
-        paths_by_categories = {}
-        for spec in all_specs:
-            for path, value in spec.items():
+        paths_by_categories: Dict[str, Dict[str, Any]] = {}
+        for one_spec in all_specs:
+            for path, value in one_spec.items():
                 for operation, definition in value.items():
                     categories = definition["tags"] if "tags" in definition and definition["tags"] else ["general"]
                     for c in categories:
@@ -83,9 +82,8 @@ class ClientGenerator(object):
 
         admin_api_spec = requests.get(f"{self.spec_root}{self.admin_spec}").json()
         paths_by_categories["admin"] = admin_api_spec["paths"]
-
-        methods_by_category = {}
-
+        methods_by_category: Dict[str, List[Dict[str, Any]]] = {}
+        
         for category, paths in paths_by_categories.items():
             methods_by_category[category] = []
             for path, value in paths.items():
@@ -97,14 +95,14 @@ class ClientGenerator(object):
                     raise ValueError("Inconsistent api version")
 
                 for operation, definition in value.items():
-                    parameters = definition["parameters"] if "parameters" in definition else []
+                    parameters: List[Dict[str, Any]] = definition["parameters"] if "parameters" in definition else []
                     path_parameters = [p["name"] for p in parameters if p["in"] == "path"]
                     query_parameters = [{"name": p["name"], "param": self._to_snake_case(p["name"])} for p in parameters if p["in"] == "query"]
 
                     method_parameters = [self._translate_parameter(p) for p in parameters]
                     method_parameters.sort(key=self._sort_params)
 
-                    method_param_names = [p["name"] for p in method_parameters]
+                    method_param_names: List[Optional[str]] = [p["name"] for p in method_parameters]
 
                     self.consolidate_request_objects(ExtendedResponseConfiguration(), method_parameters, method_param_names, query_parameters)
                     self.consolidate_request_objects(ResponseConfiguration(), method_parameters, method_param_names, query_parameters)
@@ -126,13 +124,13 @@ class ClientGenerator(object):
                         if len(generics) > 0:
                             generic_response_type = generics[0]
 
-                    method = {"operation": operation, "summary": definition["summary"] if "summary" in definition else None, "has_payload": "requestBody" in definition and definition["requestBody"],
+                    method: Dict[str, Any] = {"operation": operation, "summary": definition["summary"] if "summary" in definition else None, "has_payload": "requestBody" in definition and definition["requestBody"],
                               "path": {"name": self._translate_path(relative_path, path_parameters), "has_path_params": len(path_parameters) > 0}, "name": method_name,
                               "parameters": method_parameters, "query_parameters": query_parameters, "response_type": response_type, "generic_response_type": generic_response_type}
                     methods_by_category[category].append(method)
                     print(f"Operation: {operation}, Path: {relative_path}")
             # Todo sort by operationId
-            for cat, methods in methods_by_category.items():
+            for _, methods in methods_by_category.items():
                 methods.sort(key=lambda m: m['name'])
         with open(self.target, "w") as file:
             file.write(template.render(default_kg_root=self.default_kg_root, methods_by_category=sorted(methods_by_category.items()), api_version=api_version, id_namespace=self.id_namespace, default_client_id_for_device_flow=self.default_client_id_for_device_flow))
@@ -144,27 +142,27 @@ class ClientGenerator(object):
             return self.keyword_translations[input]
         return re.sub(r'(?<!^)(?=[A-Z])', '_', input).lower()
 
-    def _find_type(self, type, items, format, required: bool, default: str, enum) -> Optional[str]:
+    def _find_type(self, type_: Optional[str], items: Optional[Dict[str, Any]], format_: Optional[str], required: bool, default: Optional[str], enum_: Optional[List[str]]) -> Optional[str]:
         result = None
-        if type == "string":
-            if format == "uuid":
+        if type_ == "string":
+            if format_ == "uuid":
                 result = "UUID"
-            elif enum:
-                if len(enum) == 2 and "IN_PROGRESS" in enum and "RELEASED" in enum:
+            elif enum_:
+                if len(enum_) == 2 and "IN_PROGRESS" in enum_ and "RELEASED" in enum_:
                     result = "Stage = Stage.RELEASED"  # We set the default of the stage to "RELEASED"
-                elif len(enum) == 3 and "TOP_INSTANCE_ONLY" in enum and "CHILDREN_ONLY" in enum and "CHILDREN_ONLY_RESTRICTED" in enum:
+                elif len(enum_) == 3 and "TOP_INSTANCE_ONLY" in enum_ and "CHILDREN_ONLY" in enum_ and "CHILDREN_ONLY_RESTRICTED" in enum_:
                     result = "ReleaseTreeScope"
                 else:
                     result = "str"
             else:
                 result = "str"
-        elif type == "boolean":
+        elif type_ == "boolean":
             result = "bool"
-        elif type == "integer":
+        elif type_ == "integer":
             result = "int"
-        elif type == "object":
+        elif type_ == "object":
             result = "dict"
-        elif type == "array":
+        elif type_ == "array":
             if items and "type" in items and items["type"] == "string":
                 result = "List[str]"
             else:
@@ -176,42 +174,42 @@ class ClientGenerator(object):
                 result = f"Optional[{result}] = None"
         return result
 
-    def _translate_parameter(self, input: dict) -> dict:
-        schema = input["schema"] if "schema" in input else {}
-        type = schema["type"] if "type" in schema else None
-        items = schema["items"] if "items" in schema else None
-        format = schema["format"] if "format" in schema else None
-        default = str(schema["default"]) if "default" in schema else None
-        required = input["required"] if "required" in input else False
-        enum = schema["enum"] if "enum" in schema else None
+    def _translate_parameter(self, input_: Dict[str, Any]) -> Dict[str, Optional[str]]:
+        schema: Dict[str, Any] = input_.get("schema", {})
+        type_: Optional[str] = schema.get("type", None)
+        items: Optional[Dict[str, Any]] = schema.get("items", None)
+        format_: Optional[str] = schema.get("format", None)
+        default: Optional[str] = str(schema["default"]) if "default" in schema else None
+        required: bool = input_.get("required", False)
+        enum: Optional[List[str]] = schema.get("enum", None)
         return {
-            "name": self._to_snake_case(input["name"]),
-            "type": self._find_type(type, items, format, required, default, enum)
+            "name": self._to_snake_case(input_["name"]),
+            "type": self._find_type(type_, items, format_, required, default, enum)
         }
 
-    def _translate_path(self, input: str, path_params: list) -> str:
+    def _translate_path(self, input: str, path_params: List[str]) -> str:
         result = input
         for p in path_params:
             result = result.replace(f"{{{p}}}", f"{{{self._to_snake_case(p)}}}")
         return result
 
-    def _split_path(self, path):
+    def _split_path(self, path: str):
         splitted_path = path.split("/")
         api_version = splitted_path[1]
         relative_path = "/".join(splitted_path[2:])
         return api_version, relative_path
 
-    def _sort_params(self, input):
-        type = input["type"] if "type" in input else None
-        if type and "=" in type:
+    def _sort_params(self, input_: Dict[str, Any]):
+        type_ = input_.get("type", None)
+        if type_ is not None and "=" in type_:
             sort = 2
-        elif type and "Optional[" in type:
+        elif type_ is not None and "Optional[" in type_:
             sort = 1
         else:
             sort = 0
-        return f"{sort} {input['name']}"
+        return f"{sort} {input_['name']}"
 
-    def _response_type(self, responses):
+    def _response_type(self, responses: Dict[str, Dict[str, Any]]) -> Optional[str]:
         response_reference = None
         is_list = False
         if "200" in responses and responses["200"]:
@@ -219,8 +217,8 @@ class ClientGenerator(object):
                 content = None
                 if "*/*" in responses["200"]["content"] and responses["200"]["content"]["*/*"]:
                     content = responses["200"]["content"]["*/*"]
-                elif "application/json" in responses["200"]["content"] and responses["200"]["content"]["application/json"]:
-                    content = responses["200"]["content"]["application/json"]
+                elif APPLICATION_JSON in responses["200"]["content"] and responses["200"]["content"][APPLICATION_JSON]:
+                    content = responses["200"]["content"][APPLICATION_JSON]
                 if content:
                     if "schema" in content and content["schema"]:
                         schema = content["schema"]
@@ -264,9 +262,9 @@ class ClientGenerator(object):
             #     return "ListOfJsonLdDocuments" if is_list else "JsonLdDocument"
         return None
 
-    def consolidate_request_objects(self, request_object, method_parameters, method_param_names, query_parameters):
+    def consolidate_request_objects(self, request_object: object, method_parameters: List[Dict[str, Any]], method_param_names: List[Optional[str]], query_parameters: List[Dict[str, Any]]):
         all_params = True
-        request_object_name = request_object.__class__.__name__
+        request_object_name: str = request_object.__class__.__name__
         request_object_name_snake = self._to_snake_case(request_object_name)
         for r in request_object.__dict__.keys():
             if r not in method_param_names:
