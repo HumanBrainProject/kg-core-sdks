54f9028f1058e45e48f7c272756f13122e3a072e
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2022-09-12 16:39:42 +0200

    more additions and fixes

--- kgCoreJs/communication.js
+++ kgCoreJs/communication.js
@@ -41,3 +41,88 @@ export class KGConfig {
         this.enableProfiling = enableProfiling;
     }
 }
+
+export class RequestsWithTokenHandler {
+    constructor(kgConfig) {
+        if (this.constructor === RequestsWithTokenHandler) {
+            throw new Error("Class 'RequestsWithTokenHandler' cannot be instantiated");
+        }
+        this.kgConfig = kgConfig;
+        this.kgConfig.tokenHandler.defineEndpoint(this.kgConfig.endpoint);
+        if(this.kgConfig.clientTokenHandler) {
+            this.kgConfig.clientTokenHandler.defineEndpoint(this.kgConfig.endpoint);
+        }
+    }
+
+    _setHeaders(args, forceFetchToken) {
+        if(this.kgConfig.tokenHandler) {
+            const token = this.kgConfig.tokenHandler.getToken(forceFetchToken);
+            if(token) {
+                args['headers'] = {
+                    "Authorization": `"Bearer ${token}`
+                };
+            }
+            if(this.kgConfig.clientTokenHandler) {
+                const clientToken = this.kgConfig.clientTokenHandler.getToken(forceFetchToken);
+                if(clientToken){
+                    args["headers"]["Client-Authorization"] = `Bearer ${client_token}`;
+                }
+            }
+        }
+    }
+
+    _request(method, path, payload, params) {
+        const absolutePath = `${this.kgConfig.endpoint}${path}`;
+        args = {
+            'method': method,
+            'url': absolutePath,
+            'params': params
+        }
+        return this._doRequest(args, payload);
+    }
+
+    async _doRequest(args, payload) {
+        this._setHeaders(args, false);
+        if(payload) {
+            args["json"] = payload;
+        }
+        let r = await fetch(args); // TODO check how to do this in JS
+        if(r.status === 401) {
+            this._setHeaders(args, true);
+            r = await fetch(args);
+        }
+        argsClone = {...args};
+        delete argsClone["headers"];
+        let response = null;
+        try {
+            response = await r.json();
+            if(r.status >= 500) {
+                throw new Error(response); //TODO Throw a custom exception (KG Exception)
+            }
+        } catch(e) {
+            response = null;
+        }
+        return new KGRequestWithResponseContext(response, args_clone, payload, r.status_code, this.kgConfig); //TODO implement KGRequestWithResponseContext
+    }
+
+    _get(path, params) {
+        return this._request("GET", path, null, params);
+    }
+
+    _post(path, payload, params) {
+        return this._request("POST", path, payload, params);
+    }
+
+    _put(path, payload, params) {
+        return this._request("PUT", path, payload, params);
+    }
+
+    _delete(path, params) {
+        return this._request("DELETE", path, None, params);
+    }
+
+    _patch(path, payload, params) {
+        return this._request("PATCH", path, payload, params);
+    }
+
+}
