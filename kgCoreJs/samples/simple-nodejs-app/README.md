dfb759e7682837f8229a89640d648f189f57405e
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2022-10-03 14:09:42 +0200

    simple nodejs app

---
+++ kgCoreJs/samples/simple-nodejs-app/README.md
@@ -0,0 +1,11 @@
+# Simple nodejs application 
+
+## Requirements
+
+* Node >=18
+
+### Running
+
+* EXPORT TOKEN=<YOUR_TOKEN>
+
+* node --experimental-modules --es-module-specifier-resolution=node app.js
---
+++ kgCoreJs/samples/simple-nodejs-app/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { kg } from "ebrains-kg-core/kg";
+const app = express()
+const port = 3000
+
+
+const fetchToken = () => {
+  return  process.env.TOKEN;
+}
+
+const client = kg("localhost:8080").withCustomTokenProvider(fetchToken).build();
+
+app.get('/', async (req, res) => {
+  const type = req.query.type;
+  const r = await client.instances.list(type);
+  const instances = r.data;
+  res.send(instances);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
